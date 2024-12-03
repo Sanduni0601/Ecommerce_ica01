03d7a62294780b864218dccb0ee1e2762020235e
Sanduni Nisansala <139616844+Sanduni0601@users.noreply.github.com>
2024-12-03 12:32:26 +0000

    changes in Products.js

--- ecom/src/components/Products.js
+++ ecom/src/components/Products.js
@@ -1,9 +1,25 @@
-
+import React,{useState} from 'react';
 import '../assets/CSS/layout.css';
+import Product from '../components/Product';
+import Cart from '../components/Cart';
+import { flowers } from './FlowerDB';
 
 
 export default function Products(){
-   
+    const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product, quantity) => {
+    const updatedCart = [...cartItems];
+    const existingItem = updatedCart.find(item => item.id === product.id);
+
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      updatedCart.push({ ...product, quantity });
+    }
+
+    setCartItems(updatedCart);
+  };
     return(
         <>
             <div className="item1">
@@ -13,14 +29,16 @@ export default function Products(){
                 <h4 className="card-title">Buy flowers</h4>
                 <div className="grid-container">
                     {
-                        //product
+                        flowers.map((flower) => (
+                            <Product key={flower.id} product={flower} addToCart={addToCart} />
+                          ))
                     }
                 </div>
 
             </div>
             <div className="item3">
                 {
-                //cart
+                // <Cart cartItems={cartItems} />
                 }
             </div>
         </>
